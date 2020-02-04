77bf3813ce4d4f9bd1df8f2a19f96a8be83b0fb4
edward0rtiz <931@holbertonschool.com>
2020-02-04 13:36:47 -0500

    [feature] add gitignore and django_blog project up to templates

---
+++ django_blog/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Edward0rtiz',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 4, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 5, 2020'
+    }
+]
+
+# fun to handle traffic from home.
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+# fun to handle about page
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
