1086821d0a07695fd07abc5e912337eaa601273e
edward0rtiz <931@holbertonschool.com>
2020-02-25 21:04:00 -0500

    [update] the path of the new, detail-post, update, delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),  # first url mapped
+    path('', PostListView.as_view(), name='blog-home'),  # first url mapped
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # first url mapped
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # first url mapped
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
